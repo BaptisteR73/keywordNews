9ca5b97b33df8ab64929ce60b2e0d64b130d278a
BaptisteR73 <rambaudb.info@gmail.com>
2020-08-31 17:02:43 +0200

    Appel API + recup news

---
+++ route.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from flask import Flask  , jsonify
+import json
+import requests
+
+app = Flask(__name__)
+
+from functions import extract_keywords
+
+NEWS_API_KEY = "3ddb16b4b9cc4349bee1349bfbea326b"
+
+if NEWS_API_KEY is None:
+	exit()
+else:
+	NEWS_API_URL = "http://newsapi.org/v2/top-headlines?country=fr&apiKey="+NEWS_API_KEY
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/api/news/")
+def get_news():
+
+	response = requests.get(NEWS_API_URL)
+	content  =json.loads(response.content.decode('utf-8'))
+
+	keywords, articles = extract_keywords(content["articles"])
+
+	return jsonify({
+		'status': 'ok',
+		'data': {
+			'keywords' : keywords[:100],
+			'articles' : articles
+		}
+		})
+
+
+@app.route('/<name>')
+def hello_name(name):
+	return "Hello {}!".format(name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
